c33fe142a7cd63a1c28e55d7c906531c2c96b0cd
vlsidlyarevich <vlsidlyarevich@gmail.com>
2016-07-24 18:31:09 +0300

    master: path extracting

--- app/modules/server.js
+++ app/modules/server.js
@@ -1,7 +1,17 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world!");
-    response.end();
-}).listen(8080);
+function start() {
+    function onRequest(request, response) {
+        var pathName = url.parse(request.url).pathname;
+        console.log(pathName + " requested");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello world!");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8080);
+    console.log("Server has started");
+}
+
+exports.start = start;
