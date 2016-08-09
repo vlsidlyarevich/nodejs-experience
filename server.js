9563a6c7562c49cd89c67baf2dd37378af576ab6
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-08-09 23:05:43 +0300

    master: default error route added

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express = require('express')
-  , app = express()
-  , port = process.env.PORT || 3000
+var app = express()
+var url = require("url")
+var port = process.env.PORT || 8080
 
-app.get('/', function (req, res) {
-  res.send('Hello, World!')
+app.get('/', function (request, response) {
+  var pathName = url.parse(request.url).pathname;
+  console.log(pathName + " requested");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello world!");
+  response.end();
 })
 
-app.listen(port, function () {
-  console.log('Listening on port ', port)
-})
+app.use(function (request, response) {
+    response.send('404: Page not Found', 404);
+});
+
+function start(){
+  app.listen(port, function () {
+    console.log('Listening on port ', port)
+  })
+}
+
+exports.start = start;
