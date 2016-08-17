f7368bf6ce3f1c569d3176b11d2b360de3457f9b
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-08-17 22:58:35 +0300

    #12 - posts dao

--- app/db/dao/postsDAO.js
+++ app/db/dao/postsDAO.js
@@ -2,8 +2,11 @@ var mongoose = require('mongoose');
 var Post = require('../schema');
 
 function getAllPosts() {
+    console.log("*****************Finding all posts*****************");
     Post.find(function (err, posts) {
         if (err) return console.error(err);
         return posts;
     });
 }
+
+exports.getAllPosts = getAllPosts;
