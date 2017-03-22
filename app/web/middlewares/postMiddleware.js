abb57868b33dfdc96979f89075e09d548a68d089
Karina Denisevich <karina_41997@mail.ru>
2017-03-22 15:59:01 +0300

    #23 - add error handling

--- app/web/middlewares/postMiddleware.js
+++ app/web/middlewares/postMiddleware.js
@@ -3,72 +3,74 @@
 import { Post } from "../../db/models/post";
 
 function getPosts(request, response, next) {
-    const handle = (posts, error) => {
-        if (error) {
-            next(new Error("Bad request/error while obtaining posts"))
-        } else {
+    const handle = (posts) => {
             response.writeHead(200, {"Content-Type": "text/plain"});
             response.write(JSON.stringify(posts));
             response.end();
-        }
     };
 
-    Post.find().then(handle);
+    function onFailure() {
+        next(new Error("Bad request/error while obtaining posts"));
+    }
+
+    Post.find().then(handle, onFailure);
 }
 
 function getPost(request, response, next) {
-    const handle = (post, error) => {
-        if (error) {
-            next(new Error("Bad request/error while obtaining post with id: " + request.params.id));
-        } else {
+    const handle = (post) => {
             response.writeHead(200, {"Content-Type": "text/plain"});
             response.write(JSON.stringify(post));
             response.end();
-        }
+
     };
 
-    Post.getById(request.params.id).then(handle);
+    function onFailure() {
+        next(new Error("Bad request/error while obtaining post with id: " + request.params.id));
+    }
+
+    Post.getById(request.params.id).then(handle, onFailure);
 }
 
 function addPost(request, response, next) {
-    const handle = (post, error) => {
-        if (error) {
-            next(new Error("Bad request/error while saving post"));
-        } else {
+    const handle = (post) => {
             response.writeHead(200, {"Content-Type": "text/plain"});
             response.write(JSON.stringify(post));
             response.end();
-        }
     };
 
-    Post.create(request.body).then(handle);
+    function onFailure() {
+        next(new Error("Bad request/error while saving post"));
+    }
+
+    Post.create(request.body).then(handle, onFailure);
 }
 
 function updatePost(request, response, next) {
-    const handle = (post, error) => {
-        if (error) {
-            next(new Error("Bad request/error while updating post with id: " + request.params.id));
-        }
+    const handle = (post) => {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(JSON.stringify(post));
         response.end();
     };
 
-    Post.updateById(request.params.id, request.body).then(handle);
+    function onFailure() {
+        next(new Error("Bad request/error while updating post with id: " + request.params.id));
+    }
+
+    Post.updateById(request.params.id, request.body).then(handle, onFailure);
 }
 
 function deletePost(request, response, next) {
-    const handle = (post, error) => {
-        if (error) {
-            next(new Error("Bad request/error while deleting post with id: " + request.params.id));
-        } else {
+    const handle = (post) => {
             response.writeHead(200, {"Content-Type": "text/plain"});
             response.write(JSON.stringify(request.params.id));
             response.end();
-        }
     };
 
-    Post.deleteById(request.params.id).then(handle);
+    function onFailure() {
+        next(new Error("Bad request/error while deleting post with id: " + request.params.id));
+    }
+
+    Post.deleteById(request.params.id).then(handle, onFailure);
 }
 
 export { getPosts, addPost, deletePost, getPost, updatePost }
